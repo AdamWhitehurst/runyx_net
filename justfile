1554aaadb0aa45869cd7e5d15dd7fed5919a642e
Adam Whitehurst <Adam.Whitehurst@live.com>
2022-05-20 16:05:57 -0700

    🧹chore(deps): return to base workspace + bevy

--- justfile
+++ justfile
@@ -3,3 +3,8 @@ build:
 
 run MODE:
   cargo run --bin={{MODE}}
+
+host MODE:
+    cargo build --bin={{MODE}} --target wasm32-unknown-unknown
+    wasm-bindgen --out-dir public/ --target web target/wasm32-unknown-unknown/debug/{{MODE}}.wasm
+    basic-http-server
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,7 +1,4 @@
 use bevy::prelude::*;
-use bevy_renet::renet::{RenetError, NETCODE_KEY_BYTES};
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 
 /// set up a simple 3D scene
 pub fn setup(
@@ -30,40 +27,4 @@ pub fn setup(
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     });
-}
-
-pub const PRIVATE_KEY: &[u8; NETCODE_KEY_BYTES] = b"an example very very secret key."; // 32-bytes
-pub const PROTOCOL_ID: u64 = 7;
-
-pub const PLAYER_MOVE_SPEED: f32 = 1.0;
-
-#[derive(Debug, Default, Serialize, Deserialize, Component)]
-pub struct PlayerInput {
-    pub up: bool,
-    pub down: bool,
-    pub left: bool,
-    pub right: bool,
-}
-
-#[derive(Debug, Component)]
-pub struct Player {
-    pub id: u64,
-}
-
-#[derive(Debug, Default)]
-pub struct Lobby {
-    pub players: HashMap<u64, Entity>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Component)]
-pub enum ServerMessages {
-    PlayerConnected { id: u64 },
-    PlayerDisconnected { id: u64 },
-}
-
-// If any error is found we just panic
-pub fn panic_on_error_system(mut renet_error: EventReader<RenetError>) {
-    for e in renet_error.iter() {
-        panic!("{}", e);
-    }
 }
