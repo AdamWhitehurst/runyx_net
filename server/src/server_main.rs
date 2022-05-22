912ebcdf93fdde92baf865883389ee781b94c161
Adam Whitehurst <Adam.Whitehurst@live.com>
2022-05-22 11:06:51 -0700

    🚧 wip(networking): bevy-naia int

--- server/src/server_main.rs
+++ server/src/server_main.rs
@@ -1,17 +1,38 @@
-use bevy::prelude::*;
-use bevy_inspector_egui::WorldInspectorPlugin;
-use shared::*;
+use bevy_app::{App, ScheduleRunnerPlugin};
+use bevy_core::CorePlugin;
+use bevy_log::{info, LogPlugin};
+
+use naia_bevy_server::{Plugin as ServerPlugin, ServerConfig, Stage};
+
+use shared::{protocol::Protocol, shared_config, Channels};
+
+mod resources;
+mod systems;
+
+use systems::{events, init, tick};
 
 fn main() {
-    // General
-    let mut app = App::new();
-    app.add_plugins(DefaultPlugins);
+    info!("Naia Bevy Server starting up");
 
-    // Server-Only
-    app.add_plugin(WorldInspectorPlugin::new());
-
-    // Shared
-    app.add_startup_system(setup);
-
-    app.run();
+    // Build App
+    App::default()
+        // Plugins
+        .add_plugin(CorePlugin::default())
+        .add_plugin(ScheduleRunnerPlugin::default())
+        .add_plugin(LogPlugin::default())
+        .add_plugin(ServerPlugin::<Protocol, Channels>::new(
+            ServerConfig::default(),
+            shared_config(),
+        ))
+        // Startup System
+        .add_startup_system(init)
+        // Receive Server Events
+        .add_system_to_stage(Stage::ReceiveEvents, events::authorization_event)
+        .add_system_to_stage(Stage::ReceiveEvents, events::connection_event)
+        .add_system_to_stage(Stage::ReceiveEvents, events::disconnection_event)
+        .add_system_to_stage(Stage::ReceiveEvents, events::receive_message_event)
+        // Gameplay Loop on Tick
+        .add_system_to_stage(Stage::Tick, tick)
+        // Run App
+        .run();
 }
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,30 +1,10 @@
-use bevy::prelude::*;
+extern crate log;
 
-/// set up a simple 3D scene
-pub fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..Default::default()
-    });
-    // light
-    commands.spawn_bundle(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..Default::default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..Default::default()
-    });
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
-}
+pub mod behavior;
+pub mod protocol;
+
+mod channels;
+pub use channels::Channels;
+
+mod shared;
+pub use shared::shared_config;
